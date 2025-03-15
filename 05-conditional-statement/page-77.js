660121a37390aa857b3e6abdce7b92aa315c96b3
jnoyon <jihadur51@gmail.com>
2025-03-15 14:10:55 +0600

    Page 80 program added

---
+++ 05-conditional-statement/page-77.js
@@ -0,0 +1,51 @@
+// Chapter Three (Data Type) : Page No 77
+// Conditional Statement Related Problem
+// Total Practice: (This File: 6, Total: 87 + 6 = 93)
+
+const reachedHome = 7;
+if(reachedHome < 6){
+    console.log('Food for me')
+}
+else {
+    console.log('Jharur Bari')
+}
+
+const isLogin = true;
+if(isLogin){
+    console.log('Welcome')
+}
+else{
+    console.log('Get out')
+}
+
+const run = 6;
+if(run >= 5){
+    console.log('Here is your chocolate')
+}
+else{
+    console.log('Get Fat')
+}
+
+const obtainMark = 85;
+if(obtainMark > 80){
+    console.log('You will get a Bike')
+}
+else{
+    console.log('You will not get Bike')
+}
+
+const showStart = 10;
+if(showStart < 9) {
+    console.log('Show Movie')
+}
+else {
+    console.log('Sleeping')
+}
+
+const currentTemp = 30;
+if(currentTemp >=30){
+    console.log('Air Condition Start')
+}
+else {
+    console.log('Use Combol')
+}
---
+++ 05-conditional-statement/page-80.js
@@ -0,0 +1,86 @@
+// Chapter Three (Data Type) : Page No 80
+// Conditional Statement Related Problem
+// Total Practice: (This File: 9, Total: 93 + 9 = 102)
+
+const age = 19;
+const height = 62;
+if(age>18 && height> 60){
+    console.log('Push the Bus')
+}
+else {
+    console.log('Sit down on Bus')
+}
+
+const mathScore = 80;
+const englishScore = 82;
+if(mathScore > 80 || englishScore > 85){
+    console.log('You are ready to admit')
+}
+else{
+    console.log('Get ready to Marry')
+}
+
+const gpa = 5;
+const monthlyIncome = 5000;
+if((gpa == 5) && (monthlyIncome<10000)){
+    console.log('Get Schoolarship')
+}
+else{
+    console.log('Do not get Schoolarship')
+}
+
+
+const candidateAge = 28;
+const candidateExperience = 3;
+if(candidateAge<30 && candidateExperience>2){
+    console.log('Can attend Job Exam')
+}
+else {
+    console.log('Not able to attend Job Exam')
+}
+
+const egg = 12;
+const hen = true;
+if(hen==false && egg>12){
+    console.log('Cook Kurma')
+}
+else{
+    console.log('Eat Bread and Banana')
+}
+
+const temp = 102;
+const coff = true;
+if(temp>100 & coff){
+    console.log('Consult with Doctor')
+}
+else {
+    console.log('Rest in Home')
+}
+
+const attendance = 28;
+const attendanceRate = parseInt((attendance/30)*100);
+const homework = true;
+if(attendanceRate>80 && homework){
+    console.log('Can attend Exam')
+}
+else{
+    console.log('Can not attend exam and Failed')
+}
+
+const electricity = false;
+const charge = true;
+if(!electricity && !charge){
+    console.log('Read now')
+}
+else{
+    console.log('Play Video Game')
+}
+
+const shirtPrice = 5000;
+const coupon = true;
+if(shirtPrice>1000 & coupon){
+    console.log('You will get 20% Discount')
+}
+else{
+    console.log('No discount Available')
+}
