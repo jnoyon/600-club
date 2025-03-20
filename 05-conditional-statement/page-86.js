5ed8cd470938771e682e356dc82e6adc82dc6ad0
jnoyon <jihadur51@gmail.com>
2025-03-20 15:57:43 +0600

    Chapter Five Completed

---
+++ 05-conditional-statement/page-86.js
@@ -0,0 +1,75 @@
+// Chapter Three (Data Type) : Page No 86
+// Conditional Statement Related Problem
+// Total Practice: (This File: 5, Total: 109 + 5 = 114)
+
+const invitation = false;
+const amount = 100;
+if(invitation){
+    
+    if(amount > 1000){
+        console.log("Need to buy a Gift")
+    }
+    else{
+        console.log("Not enough money to buy a Gift")
+    }
+}
+else {
+    console.log("Unfriend Him")
+}
+
+const isDrinkTea = true;
+const withBiscuits = false;
+if(isDrinkTea){
+    if(withBiscuits){
+        console.log("Tea and Biscuits Ready")
+    }
+    else{
+        console.log("Tea Ready")
+    }       
+}
+else{
+    console.log("Watch Star Ajaira")
+}
+
+
+const isActive = false;
+isSubscribe = false;
+if(isActive){
+    if(isSubscribe){
+        console.log("Watch Premium Content")
+    }
+    else{
+        console.log("See Free Content")
+    }
+}
+else{
+    console.log("Your account is not Active")
+}
+
+const isFoodAvailabe = true;
+const isFoodAppWorking = false;
+if(isFoodAvailabe){
+    if(isFoodAppWorking){
+        console.log("Order Food")
+    }
+    else{
+        console.log("Make it Warm")
+    }
+}
+else{
+    console.log("Fasting")
+}
+
+const totalAttendance = 100;
+const gift= true;
+if(totalAttendance>100){
+    if(gift){
+        console.log("Lets Party all Night");
+    }
+    else{
+        console.log("I will party with myself")
+    }
+}
+else{
+    console.log("No party")
+}
---
+++ 05-conditional-statement/page-90.js
@@ -0,0 +1,24 @@
+// Chapter Three (Data Type) : Page No 90
+// Conditional Statement Related Problem
+// Total Practice: (This File: 7, Total: 114 + 7 = 121)
+
+const purchase = 20000;
+const cashback = purchase>3000? 500 : 0;
+
+const age =10;
+age>15 ? console.log("Teneger") : console.log("Child")
+
+const login = true;
+login? console.log("Welcome Back") : console.log("Please Login")
+
+const tankFull = false;
+tankFull? console.log("Ready for a long drive"): console.log("Fill the Tank")
+
+const isPassed = true;
+isPassed? console.log("Party Time: 10.00") : console.log("Next Semester e Serious Study korbo");
+
+const sunny = false;
+!sunny && console.log("Stay Home")
+
+const expensive = false;
+!expensive && console.log("I will buy this item")
