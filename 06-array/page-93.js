cdc7b0fb5053941665e340d871ad68e47598a630
jnoyon <jihadur51@gmail.com>
2025-03-22 12:57:21 +0600

    Page-93 Problems Added

---
+++ 06-array/page-93.js
@@ -0,0 +1,18 @@
+// Chapter Six (Array) : Page No 93
+// Total Practice: (This File: 7, Total: 121 + 7 = 128)
+
+const number = [71, 72, 73, 74, 75, 76, 77, 78, 79]
+
+const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Guava']
+
+const items = ['alu', 'peyaj', 'begun', 'mula', 'gajor', 'tomato', 'dheros', 'lao', 'sosa', 'potol']
+console.log(items.length);
+
+const cinema = ['cinema1', 'cinema2', 'cinema3', 'cinema4', 'cinema5']
+
+const oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
+
+const colors = ['green', 'purple', 'black', 'red', 'purple']
+
+const capitals = ['Dhaka', 'London', 'Delhi', 'Karachi']
+console.log(capitals.length)
