adf952ba6c6de81f0f3fc70e30a4cda513dc7b77
jnoyon <jihadur51@gmail.com>
2025-03-22 13:27:22 +0600

    Page 96 Problems Solve

---
+++ 06-array/page-96.js
@@ -0,0 +1,24 @@
+// Chapter Six (Array) : Page No 96
+// Total Practice: (This File: 7, Total: 128 + 7 = 135)
+
+const friends = ['Arif', 'Jamal', 'Kamal'];
+console.log(friends.indexOf('Jamal'));
+
+const books = ['Mission Islam', 'Mumin er Carrier Bhabna', 'Moner Moto Salat', 'Bibah Path', 'Ar Rahikul Makhtum', 'Message', 'Rahe Belayet']
+console.log(books[5])
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers[7] = 30;
+console.log(numbers[7])
+
+const games = ['Free Fire', 'Pubg', 'Candy Crush', 'Temple Run']
+games[2] = 'Subway Surfer';
+
+const familyMembers = ['Fatema', 'Shiuly', 'Jibon', 'Noyon']
+console.log(familyMembers[2])
+
+const country = ['Saudi Arabia', 'Pakistan', 'India', 'China', 'England', 'Morokko', 'Afganistan']
+console.log(country[3])
+
+const tableElements = ['Mobile', 'Books', 'HeadPhone', 'Microphone']
+console.log(tableElements[6])
