4898cd4fc312a07891b61021253c45ee214d4819
jnoyon <jihadur51@gmail.com>
2025-03-25 00:28:00 +0600

    page - 126 updated

---
+++ 08-object/page-126.js
@@ -0,0 +1,52 @@
+// Chapter Eight (Object) : Page No 126
+// Total Practice: (This File: 8, Total: 176 + 8 = 184)
+
+const teacher = {
+    name : 'Jihadur Rahman',
+    subject: 'ICT',
+    age : 27,
+    address : 'Bhaluka'
+}
+
+const tree = {
+    name: 'Mango Tree',
+    height : '10 Feet',
+    age :  10,
+    fruit: 'Mango'
+}
+const mosha = {
+    name: 'Mosquito',
+    age : 'Unknown',
+    gender: 'Female',
+    legs : 6
+}
+
+const mother = {
+    name : 'Fatema',
+    occupation : 'Homemaker',
+    age: 52,
+    childrens: 3
+}
+
+const bike = {
+    name: 'Hero',
+    tyre: 2,
+    color: 'Red',
+    maxSpeed: '120kmph',
+    price: 156000
+}
+
+const bird = {
+    name: 'Pigeon',
+    color: 'white',
+    food: 'Bird Mixture',
+    price: 1200
+}
+
+const laptop = {
+    name: 'HP',
+    processor: 'Core i3 6th Gen',
+    ram: '8GB',
+    price: 32000,
+    display: 14.1
+}
