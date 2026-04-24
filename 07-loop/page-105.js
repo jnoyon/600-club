56a5fc502034a3d4c191f647aaba457790a011f3
jnoyon <jihadur51@gmail.com>
2026-04-24 21:22:25 +0600

    Chapter Seven Added

---
+++ 07-loop/page-105.js
@@ -0,0 +1,42 @@
+/*
+Chapter Name: Array
+Page No: 105
+This File: 5
+Total Practice: 150
+*/
+
+const favSubjects = ["Bangla", "English", "ICT", "Physics", "Islamic Studies"];
+for (let favSubject of favSubjects) {
+  console.log(favSubject);
+}
+
+const favFoods = ["Biriyani", "Khacchi", "Khichuri"];
+for (let favFood of favFoods) {
+  console.log(favFood);
+}
+
+const birthYears = [1971, 1990, 1995, 1998];
+for (let birthYear of birthYears) {
+  console.log(birthYear);
+}
+const players = [
+  "Tamim",
+  "Tanjid",
+  "Shakib",
+  "Mahmudullah",
+  "Musfiq",
+  "Mehedi Miraz",
+  "Saifuddin",
+  "Mashrafee",
+  "Taskin",
+  "Rubel",
+  "Mostafizur",
+];
+for (let player of players) {
+  console.log(player);
+}
+
+const exams = [21, 23, 26, 28, 30];
+for (let exam of exams) {
+  console.log(exam);
+}
---
+++ 07-loop/page-109.js
@@ -0,0 +1,36 @@
+/*
+Chapter Name: Array
+Page No: 109
+This File: 5
+Total Practice: 155
+*/
+
+let a = 0;
+while (a < 10) {
+  console.log("I will code minimum 3 hours daily");
+  a++;
+}
+
+let start = 1;
+while (start <= 20) {
+  console.log(start);
+  start++;
+}
+
+let num = 50;
+while (num <= 100) {
+  console.log(num);
+  num++;
+}
+
+let i = 1;
+while (i <= 10) {
+  console.log(i * 3);
+  i++;
+}
+
+let n = 111;
+while (n <= 210) {
+  console.log(n / 2);
+  n++;
+}
